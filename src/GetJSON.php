041fc8084f12364044eef510262cbe1d1373e7be
Tioranat.info <62396828+Tioranat@users.noreply.github.com>
2020-04-07 21:11:48 +0200

    new function names

--- src/GetJSON.php
+++ src/GetJSON.php
@@ -5,16 +5,32 @@ namespace Tioranat\Controller\Translation;
 
 class GetJSON {
     
-    static function getSurvey(){
-        return __DIR__. "/../rki/survey.json";
+    static function getSurveyFileName() {
+        return __DIR__ . "/../rki/survey.json";
     }
     
-    static function getTranslation($Locale,$File){
-        $FileName = __DIR__."/../locale/$Locale/$File.json";
-        if(file_exists($FileName)){
+    static function getTranslationFileName($Locale, $File) {
+        $FileName = __DIR__ . "/../lang/$Locale/$File.json";
+        if (file_exists($FileName)) {
             return $FileName;
-        }else{
+        } else {
             return false;
+        }
+    }
+    
+    static function readAsJSON($FileNameJSON) {
+        if (file_exists($FileNameJSON)) {
+            return file_get_contents($FileNameJSON);
+        }
+        return false;
+    }
+    
+    static function readAsPHP($FileNameJSON) {
+        $JSON = self::readAsJSON($FileNameJSON);
+        if ($JSON) {
+            return json_decode($JSON, true);
+        } else {
+            return [];
         }
     }
 }
